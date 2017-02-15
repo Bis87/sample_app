59fb60c2defa82c65791f426b7d8bf9652326836
Bis87 <pavel.rytvinsky@gmail.com>
2017-02-15 23:06:13 +0300

    filling_in_layout finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+
+  get "users/new"
+  # get "static_pages/home"
+  # get "static_pages/help"
+  # get "static_pages/about"
+  # get "static_pages/contact"
+
+  # code above is equal to code below
+
+  root  'static_pages#home'                                # link: root_path
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get' # page link looks like: help_path
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
